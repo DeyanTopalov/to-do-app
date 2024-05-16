7fe9ebfcf8b063d9bcf7322180b60afc3f71b1f0
DeyanTopalov <deyan.topalow@gmail.com>
2024-05-16 22:11:26 +0300

    some upd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import ToDoCard from "@components/ToDoCard";
-import ToDoTest from "@components/todotest";
 import LocalStorageDemo from "@components/LocalStorageTest";
 export default function Home() {
   return (
